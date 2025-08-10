de64028106c62a68059a2a9300b79300f0a03f33
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-11 03:24:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { wagmiConfig } from './lib/wagmi'
+import App from './App'
+
+console.log('🔧 Setting up wagmi for reward claimer contract');
+
+const queryClient = new QueryClient()
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
