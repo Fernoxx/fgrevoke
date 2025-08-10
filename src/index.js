b32701cdc28f9a3950cc52877c9ba99c110090c4
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-11 03:23:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 console.log('🔧 Setting up wagmi for reward claimer contract');
