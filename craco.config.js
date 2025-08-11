8a23ac0919474e7212fcd57037075d055aa530ea
Ferno <154271878+Fernoxx@users.noreply.github.com>
2025-08-12 03:53:37 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -14,7 +14,6 @@ module.exports = {
         "assert": require.resolve("assert/"),
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify/browser"),
       };
 
       // Add plugins for polyfills
